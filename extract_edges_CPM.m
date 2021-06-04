d86ffcac2d9795cbb88d62396c5740521174fe0f
rorytboyle <rorytboyle@gmail.com>
2021-06-04 12:02:25 +0100

    Add files via upload

--- extract_edges_CPM.m
+++ extract_edges_CPM.m
@@ -1,5 +1,5 @@
 function [pos_edges_all, neg_edges_all, pos_edges_thresh, neg_edges_thresh] = ...
-    extract_edges_CPM(pos_mask_all, neg_mask_all, no_node, k, freq_thresh)
+    extract_edges_CPM(pos_mask_all, neg_mask_all, no_node, k_all, freq_thresh)
 % Extracts edges that were selected in all folds and in a specified %
 % of folds in both positive and network strength models.
 %
@@ -7,7 +7,9 @@ function [pos_edges_all, neg_edges_all, pos_edges_thresh, neg_edges_thresh] = ..
 % pos_mask_all =        as described in run_flexible_CPM.
 % neg_mask_all =        as described in run_flexible_CPM.
 % no_node =             as described in CPM_prep_arrays. 
-% k =                   as described in run_flexible_CPM.
+% k_all =               number of folds across all iterations. If k = n 
+%                       (LOOCV) and 1 iteration run, then k_all = n. If k =
+%                       10 and 100 iterations run, then k_all = 1000.
 % freq_thresh =         (double) frequency threshold for edge selection. If
 %                       freq_thresh = 100, edge must be selected (i.e.
 %                       significantly correlated with target variable) in
@@ -35,6 +37,7 @@ function [pos_edges_all, neg_edges_all, pos_edges_thresh, neg_edges_thresh] = ..
 % Author: Rory Boyle
 % Contact: rorytboyle@gmail.com
 % Date: 25/01/2021
+% Updated: 31/05/2021 (
 %
 % Preallocate arrays
 pos_edge_freq = zeros(no_node, no_node);
@@ -43,6 +46,8 @@ neg_edge_freq = zeros(no_node, no_node);
 % Loop through masks and sum edges across folds (i.e. if k/number of folds
 % = 10 and an edge was selected in all folds, then edge will have total
 % value of 10).
+% RB 31/05/2021 - COULD SPEED THIS BY USING sum(pos_mask_all, 3);
+% sum(neg_mask_all,3);
 for i = 1:no_node
     for j = 1:no_node
         pos_edge_freq(i,j) = sum(pos_mask_all(i, j, :));
@@ -66,7 +71,7 @@ pos_edges_all = [freqs_pos pos_lin_ix pos_row pos_col];
 neg_edges_all = [freqs_neg neg_lin_ix neg_row neg_col];
 
 % calculate required number of folds from frequency threshold
-folds_thresh = k * (freq_thresh/100);
+folds_thresh = k_all * (freq_thresh/100);
 
 % get thresholded edges
 pos_edges_thresh = pos_edges_all((pos_edges_all(:,1) >= folds_thresh),:);
